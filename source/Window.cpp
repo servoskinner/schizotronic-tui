0150d1b9aa4d582345ec489642b8c55b395cc937
Fyodor <egorovf9@gmail.com>
2025-04-15 12:25:33 +0300

    fix window, update cmakelists

--- source/Window.cpp
+++ source/Window.cpp
@@ -2,10 +2,11 @@
 
 void Window::draw_self(TUI& tui, unsigned input, Vector2i origin) {
     screen_size = tui.get_screen_size();
+
     if (fill.background != TUI::Color::TRANSPARENT || 
         (fill.foreground != TUI::Color::TRANSPARENT && fill.symbol != ' ')) {
         for (int x = 0; x < screen_size.x; x++) {
-            for (int y = 0; x < screen_size.y; y++) {
+            for (int y = 0; y < screen_size.y; y++) {
                 tui.draw_glyph({x, y}, fill);
             }
         }
